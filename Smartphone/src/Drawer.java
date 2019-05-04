e421bcd00a6870a4beb911c7afea0cd33abfc9ca
David <david.crittin@gmail.com>
2019-05-04 10:47:10 +0200

    Push pour synchro avec Sylvain

---
+++ Smartphone/src/Drawer.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Drawer extends JPanel {
+    public Drawer() {
+
+        JLabel lblTest = new JLabel("DRAWER");
+        add(lblTest);
+    }
+}
