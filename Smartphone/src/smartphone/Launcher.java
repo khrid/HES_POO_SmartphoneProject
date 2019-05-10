53db60e76afff854d3f69c5ab5a8055c9bcbe662
Sylvain <celermeyer1994@gmail.com>
2019-05-10 13:36:38 +0200

    Ajout du bouton home + changement emplacement bouton Drawer

--- Smartphone/src/smartphone/Launcher.java
+++ Smartphone/src/smartphone/Launcher.java
@@ -2,12 +2,42 @@ package smartphone;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Launcher extends JPanel {
+
+    private JButton btnDrawer = new JButton("Show apps");
+    private Drawer drawer = new Drawer();
+
     public Launcher() {
         System.out.println("smartphone.Launcher created");
         JLabel lblTest = new JLabel("LAUNCHER");
         add(lblTest);
         setBackground(Color.RED);
+
+        DrawerListener dl = new DrawerListener();
+        btnDrawer.addActionListener(dl);
+
+        add(btnDrawer, BorderLayout.SOUTH);
+    }
+
+    public class DrawerListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(!drawer.isVisible()) {
+                //remove(launcher);
+                add(drawer);
+                btnDrawer.setText("Hide apps");
+            } else {
+                remove(drawer);
+                //add(launcher);
+                btnDrawer.setText("Show apps");
+            }
+            drawer.setVisible(!drawer.isVisible());
+            revalidate();
+            repaint();
+        }
     }
 }
